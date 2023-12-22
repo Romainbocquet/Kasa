0c87831fec5c29154505327c6acbce7d64bf50f0
Romainbocquet <94678672+Romainbocquet@users.noreply.github.com>
2023-12-22 10:45:32 +0100

    fix css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Root from './Routes/Root';
-import '../src/styles/index.css';
+import '../src/styles/index.scss';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
